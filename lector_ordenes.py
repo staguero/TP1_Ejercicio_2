a331959ccf217058c4f13f38374ff2a18af29dd5
francoleyes <85895403+francoleyes@users.noreply.github.com>
2022-05-09 19:17:50 -0300

    Add files via upload

--- lector_ordenes.py
+++ lector_ordenes.py
@@ -1,26 +1,25 @@
-def lector_ordenes(longitud_de_orden):
+def lector_ordenes():
     archivo=open('ordenes.txt',"r")
     lista_archivo=archivo.readlines()
-
-    lista_momentanea=[]
     lista_final=[]
-    contador=0 #lo uso para saber cuantas ordenes hay
-
+    posiciones = []
     for linea in lista_archivo:
         if "Order" in linea:
-            posicion=lista_archivo.index(linea)
-            contador+=1
-            for i in range(posicion+1,posicion+longitud_de_orden+1):
-                lista_momentanea.append(lista_archivo[i])
+            posiciones.append(lista_archivo.index(linea))
 
-    for i in range(0,len(lista_momentanea),longitud_de_orden):
-        lista_final.append(lista_momentanea[i:i+longitud_de_orden])   
-    
-    #Borrado de caracteres
-    caracteres="P \n"
-    for mapa in lista_final:
-        for i in range(0,longitud_de_orden):
-            for x in range(len(caracteres)):
-                mapa[i]=mapa[i].replace(caracteres[x],"")
+    for k in range (len(posiciones)-1):
+        lista_momentanea=[]
+        for i in range(posiciones[k]+1,posiciones[k+1]-1):
+            lista_archivo[i]=lista_archivo[i].replace("P","")
+            lista_archivo[i]=lista_archivo[i].replace("\n","")
+            lista_momentanea.append(lista_archivo[i])
+        lista_final.append(lista_momentanea)
+
+    lista_momentanea=[]
+    for i in range(posiciones[len(posiciones)-1]+1,len(lista_archivo)):
+        lista_archivo[i]=lista_archivo[i].replace("P","")
+        lista_archivo[i]=lista_archivo[i].replace("\n","")
+        lista_momentanea.append(lista_archivo[i])
+    lista_final.append(lista_momentanea)
 
     return lista_final
--- main_alg_gen.py
+++ main_alg_gen.py
@@ -6,12 +6,12 @@ def dibujo():
         print(mapa_dibujo[i])
 
 columnas_estante=3
-estantes_f=3
-estantes_c=3
+estantes_f=5
+estantes_c=5
 mapa_filas = 2*estantes_f+4+(estantes_f-1)*2
 mapa_columnas= estantes_c*columnas_estante+4+(estantes_c-1)*2
 
-orders = lector_ordenes(10)
+orders = lector_ordenes()
 for i in orders:
     i.insert(0,20000)
     i.append(20000)
@@ -19,7 +19,7 @@ for i in orders:
 n_shelfs = columnas_estante*2*estantes_c*estantes_f
 n_individuals = 10
 object=Genetic_Algorithm(n_shelfs,n_individuals,orders)
-best_individual = object.start(10,columnas_estante,estantes_f,estantes_c)
+best_individual = object.start(40,columnas_estante,estantes_f,estantes_c)
 print("Mejor individuo")
 print(best_individual)
 mapa=crear_mapa(columnas_estante,estantes_f,estantes_c,best_individual)
